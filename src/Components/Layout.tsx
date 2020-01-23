47e9dad42eeeb3e9d7e7b1cc43b2ba09531caa68
Matheus Moura <matheus.moura13@gmail.com>
2020-01-23 14:09:41 -0300

    Cria componente do menu

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import Menu from './Menu'
+import Grid from '@material-ui/core/Grid'
+
 /**
  * Base de todas as páginas, utilize como componente pai
  */
 const Layout: React.FC = () => {
-  return <div>Olá mundo</div>
+  return (
+    <Grid container>
+      <Grid item>
+        <Menu />
+      </Grid>
+      <Grid item>container</Grid>
+    </Grid>
+  )
 }
 
 export default Layout
