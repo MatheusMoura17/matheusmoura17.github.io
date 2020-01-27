99bf865fd6f4aa23d15f7ce3386cfe890757ed19
Matheus Moura <matheus.moura13@gmail.com>
2020-01-27 03:05:41 -0300

    Adiciona informações sobre todos os projetos

--- src/services/projects/index.ts
+++ src/services/projects/index.ts
@@ -1,5 +1,29 @@
 const westCombat = require('./west-combat.json') as IProject
+const wbolh = require('./wbolh.json') as IProject
+const recifeAdventures = require('./recife-adventures.json') as IProject
+const salveRecife = require('./salve-recife.json') as IProject
+const starDonkeys = require('./star-donkeys.json') as IProject
+const boyDoBote = require('./boy-do-bote.json') as IProject
+const embaixadinha = require('./embaixadinha.json') as IProject
+const qualEANota = require('./qual-e-a-nota.json') as IProject
+const speedBall3d = require('./speed-ball-3d.json') as IProject
+const oPequenoUrubu = require('./o-pequeno-urubu.json') as IProject
+const enemRunner = require('./enem-runner.json') as IProject
+const gremlinJump = require('./gremlin-jump.json') as IProject
 
-const projects = [westCombat]
+const projects = [
+  westCombat,
+  wbolh,
+  recifeAdventures,
+  salveRecife,
+  starDonkeys,
+  boyDoBote,
+  embaixadinha,
+  qualEANota,
+  speedBall3d,
+  oPequenoUrubu,
+  enemRunner,
+  gremlinJump,
+]
 
 export default projects
